3c280f24d71f568fa703177f5ea8038bd766c96c
Taras Marchuk <tarlinius@gmail.com>
2019-03-23 00:30:45 +0200

    lection 187 comment form

--- src/components/CommentListContainer.js
+++ src/components/CommentListContainer.js
@@ -3,9 +3,11 @@ import {commentListFetch, commentListUnload} from "../actions/actions";
 import {connect} from "react-redux";
 import {Spinner} from "./Spinner";
 import {CommentList} from "./CommentList";
+import CommentForm from "./CommentForm";
 
 const mapStateToProps = state => ({
-    ...state.commentList
+    ...state.commentList,
+    isAuthenticated: state.auth.isAuthenticated
 });
 const mapDispatchToProps = {
     commentListFetch,
@@ -22,13 +24,18 @@ class CommentListContainer extends React.Component {
     }
 
     render() {
-        const {isFetching, commentList} = this.props;
+        const {isFetching, commentList, isAuthenticated} = this.props;
 
         if (isFetching) {
             return (<Spinner/>);
         }
 
-        return (<CommentList commentList={commentList} />);
+        return (
+            <div>
+                <CommentList commentList={commentList} />
+                {isAuthenticated && <CommentForm/>}
+            </div>
+        );
     }
 }
 
